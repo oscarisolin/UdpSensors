6dcdb7f07331449d1bd6d74efb2a01f7c8e43f3b
oscar weissenbach <oscar.infos@gmail.com>
2021-03-24 17:20:59 +0100

    Added Accelerometer Sensor with text output

--- app/src/main/java/com/example/udpsensors/MainActivity.java
+++ app/src/main/java/com/example/udpsensors/MainActivity.java
@@ -1,15 +1,28 @@
 package com.example.udpsensors;
 
-import android.support.design.widget.TextInputEditText;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
+import android.util.Log;
 import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.Switch;
-import android.widget.TextClock;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    private SensorManager sensorManager;
+    private final float[] accelerometerReading = new float[3];
+    private Sensor accelerometer;
+
+    private TextView accel_y;
+    private TextView accel_z;
+    private TextView accel_x;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,25 +30,62 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Switch enable_udp_switch = findViewById(R.id.enable_udp);
-        enable_udp_switch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                EditText addressIP = findViewById(R.id.EnterIPAddress);
-                EditText addressPort = findViewById(R.id.EnterPortNumber);
-                if (isChecked){
-                    addressPort.setText(addressIP.getText());
-                }else{
-                    addressPort.setText("");
-                }
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        accel_y = findViewById(R.id.accel_y);
+        accel_z = findViewById(R.id.accel_z);
+        accel_x = findViewById(R.id.accel_x);
+
+        enable_udp_switch.setOnCheckedChangeListener((enable_udp, isChecked) -> {
+            EditText addressIP = findViewById(R.id.EnterIPAddress);
+            EditText addressPort = findViewById(R.id.EnterPortNumber);
+            if (isChecked){
+                addressPort.setText(addressIP.getText());
+                sensorManager.registerListener(MainActivity.this,accelerometer,SensorManager.SENSOR_DELAY_NORMAL);
+                Log.d("SENSORS", "Started Listener");
+            }else{
+                addressPort.setText("");
+                sensorManager.unregisterListener(MainActivity.this);
+                Log.d("SENSORS", "Stopped Listener");
             }
         });
     }
 
-    public void startSending(View view) {
 
-        EditText addressIP = findViewById(R.id.EnterIPAddress);
-        EditText addressPort = findViewById(R.id.EnterPortNumber);
 
-        addressPort.setText(addressIP.getText());
+
+
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            System.arraycopy(event.values, 0, accelerometerReading,
+                    0, accelerometerReading.length);
+
+
+            accel_x.setText(String.format("value: %.5f", accelerometerReading[0]));
+            accel_y.setText(String.format("value: %.5f", accelerometerReading[1]));
+            accel_z.setText(String.format("value: %.5f", accelerometerReading[2]));
+
+        }
+    }
+
 }
